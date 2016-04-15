b25baa0d1a097281b9daa131fc5332c356b0b490
siebert <looijesiebert@gmail.com>
2016-04-15 12:32:17 +0200

    changed the content type handler

--- src/main/java/edu/uci/ics/crawler4j/parser/Parser.java
+++ src/main/java/edu/uci/ics/crawler4j/parser/Parser.java
@@ -44,158 +44,206 @@ import edu.uci.ics.crawler4j.util.Util;
 /**
  * @author Yasser Ganjisaffar
  */
-public class Parser extends Configurable {
+public class Parser extends Configurable
+{
 
-  protected static final Logger logger = LoggerFactory.getLogger(Parser.class);
+    protected static final Logger logger = LoggerFactory.getLogger(Parser.class);
 
-  private final HtmlParser htmlParser;
-  private final ParseContext parseContext;
+    private final HtmlParser htmlParser;
+    private final ParseContext parseContext;
 
-  public Parser(CrawlConfig config) {
-    super(config);
-    htmlParser = new HtmlParser();
-    parseContext = new ParseContext();
-  }
-
-  public void parse(Page page, String contextURL) throws NotAllowedContentException, ParseException {
-    if (Util.hasBinaryContent(page.getContentType(), page.getContentData())) { // BINARY
-      BinaryParseData parseData = new BinaryParseData();
-      if (config.isIncludeBinaryContentInCrawling()) {
-        if (config.isProcessBinaryContentInCrawling()) {
-          parseData.setBinaryContent(page.getContentData());
-        } else {
-          parseData.setHtml("<html></html>");
-        }
-        page.setParseData(parseData);
-        if (parseData.getHtml() == null) {
-          throw new ParseException();
-        }
-        parseData.setOutgoingUrls(Net.extractUrls(parseData.getHtml()));
-      } else {
-        throw new NotAllowedContentException();
-      }
-    } else if (Util.hasPlainTextContent(page.getContentType())) { // plain Text
-      try {
-        TextParseData parseData = new TextParseData();
-        if (page.getContentCharset() == null) {
-          parseData.setTextContent(new String(page.getContentData()));
-        } else {
-          parseData.setTextContent(new String(page.getContentData(), page.getContentCharset()));
-        }
-        parseData.setOutgoingUrls(Net.extractUrls(parseData.getTextContent()));
-        page.setParseData(parseData);
-      } catch (Exception e) {
-        logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
-        throw new ParseException();
-      }
-    } else { // isHTML
-      Metadata metadata = new Metadata();
-      HtmlContentHandler contentHandler = new HtmlContentHandler();
-      try (InputStream inputStream = new ByteArrayInputStream(page.getContentData())) {
-        htmlParser.parse(inputStream, contentHandler, metadata, parseContext);
-      } catch (Exception e) {
-        logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
-        throw new ParseException();
-      }
-
-      if (page.getContentCharset() == null) {
-        page.setContentCharset(metadata.get("Content-Encoding"));
-      }
-      
-      // Handle canonical URLs as redirect, according to:
-      // https://support.google.com/webmasters/answer/139066?hl=en&rd=1
-      if (contentHandler.getCanonicalUrl() != null) {
-        String url = URLCanonicalizer.getCanonicalURL(contentHandler.getCanonicalUrl(), contextURL);
-        if (url != null && !url.equals(page.getWebURL().getURL())) {
-          page.setRedirect(true);
-          page.setRedirectedToUrl(url);
-          
-          // Do parse the retrieved content for additional links
-        }        
-      }
-      
-      // Handle fragment meta tag
-      // According to: https://developers.google.com/webmasters/ajax-crawling/docs/specification
-      // When a page contains a <meta name="fragment" content="!"> tag, this indicates
-      // that the page content is loaded through AJAX and a crawler that does not
-      // parse Javascript should request the same page with _escaped_fragment_=
-      // as a query parameter to get the appropriate content.
-      String fragment = contentHandler.getMetaTags().get("fragment");
-      if (fragment != null && "!".equals(fragment))
-      {
-          String currentUrl = page.getWebURL().getURL();
-          if (!currentUrl.contains("_escaped_fragment_"))
-          {
-              char connector = '?';
-              if (currentUrl.contains("&"))
-                  connector = '&';
-              String url = currentUrl + connector + "_escaped_fragment_=";
-              page.setRedirect(true);
-              page.setRedirectedToUrl(url);
-          
-              // Do parse the retrieved content for additional links
-          }
-      }
-
-      HtmlParseData parseData = new HtmlParseData();
-      parseData.setText(contentHandler.getBodyText().trim());
-      parseData.setTitle(metadata.get(DublinCore.TITLE));
-      parseData.setMetaTags(contentHandler.getMetaTags());
-      // Please note that identifying language takes less than 10 milliseconds
-      LanguageIdentifier languageIdentifier = new LanguageIdentifier(parseData.getText());
-      page.setLanguage(languageIdentifier.getLanguage());
-
-      Set<WebURL> outgoingUrls = new HashSet<>();
-
-      String baseURL = contentHandler.getBaseUrl();
-      if (baseURL != null) {
-        contextURL = baseURL;
-      }
-
-      int urlCount = 0;
-      for (ExtractedUrlAnchorPair urlAnchorPair : contentHandler.getOutgoingUrls()) {
-
-        String href = urlAnchorPair.getHref();
-        if ((href == null) || href.trim().isEmpty()) {
-          continue;
-        }
-
-        String hrefLoweredCase = href.trim().toLowerCase();
-        if (!hrefLoweredCase.contains("javascript:") && !hrefLoweredCase.contains("mailto:") &&
-            !hrefLoweredCase.contains("@")) {
-          String url = URLCanonicalizer.getCanonicalURL(href, contextURL);
-          if (url != null) {
-            WebURL webURL;
-            try {
-              webURL = new WebURL(url);
-            } catch (URISyntaxException e) {
-              logger.debug("Skipping invalid URI: {}", url);
-              continue;
-            }
-            webURL.setTag(urlAnchorPair.getTag());
-            webURL.setAnchor(urlAnchorPair.getAnchor());
-            outgoingUrls.add(webURL);
-            urlCount++;
-            if (urlCount > config.getMaxOutgoingLinksToFollow()) {
-              break;
-            }
-          }
-        }
-      }
-      parseData.setOutgoingUrls(outgoingUrls);
-
-      try {
-        if (page.getContentCharset() == null) {
-          parseData.setHtml(new String(page.getContentData()));
-        } else {
-          parseData.setHtml(new String(page.getContentData(), page.getContentCharset()));
-        }
-
-        page.setParseData(parseData);
-      } catch (UnsupportedEncodingException e) {
-        logger.error("error parsing the html: " + page.getWebURL().getURL(), e);
-        throw new ParseException();
-      }
+    public Parser(CrawlConfig config)
+    {
+        super(config);
+        htmlParser = new HtmlParser();
+        parseContext = new ParseContext();
     }
-  }
+
+    public void parse(Page page, String contextURL) throws NotAllowedContentException, ParseException
+    {
+        String type = Util.getContentType(page.getContentType(), page.getContentData());
+        if (type == "Binary" || type == "XML" || type == "Unknown")
+        { // BINARY
+            BinaryParseData parseData = new BinaryParseData();
+            if (config.isIncludeBinaryContentInCrawling())
+            {
+                if (config.isProcessBinaryContentInCrawling())
+                {
+                    parseData.setBinaryContent(page.getContentData());
+                }
+                else
+                {
+                    parseData.setHtml("<html></html>");
+                }
+                page.setParseData(parseData);
+                if (parseData.getHtml() == null)
+                {
+                    throw new ParseException();
+                }
+                parseData.setOutgoingUrls(Net.extractUrls(parseData.getHtml()));
+            }
+            else
+            {
+                throw new NotAllowedContentException();
+            }
+        }
+        else if (type == "Plaintext")
+        { // plain Text
+            try
+            {
+                TextParseData parseData = new TextParseData();
+                if (page.getContentCharset() == null)
+                {
+                    parseData.setTextContent(new String(page.getContentData()));
+                }
+                else
+                {
+                    parseData.setTextContent(new String(page.getContentData(), page.getContentCharset()));
+                }
+                parseData.setOutgoingUrls(Net.extractUrls(parseData.getTextContent()));
+                page.setParseData(parseData);
+            }
+            catch (Exception e)
+            {
+                logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
+                throw new ParseException();
+            }
+        }
+        else if (type == "Html" || type == "WhiteList")
+        { // isHTML
+            Metadata metadata = new Metadata();
+            HtmlContentHandler contentHandler = new HtmlContentHandler();
+            try (InputStream inputStream = new ByteArrayInputStream(page.getContentData()))
+            {
+                htmlParser.parse(inputStream, contentHandler, metadata, parseContext);
+            }
+            catch (Exception e)
+            {
+                logger.error("{}, while parsing: {}", e.getMessage(), page.getWebURL().getURL());
+                throw new ParseException();
+            }
+
+            if (page.getContentCharset() == null)
+            {
+                page.setContentCharset(metadata.get("Content-Encoding"));
+            }
+
+            // Handle canonical URLs as redirect, according to:
+            // https://support.google.com/webmasters/answer/139066?hl=en&rd=1
+            if (contentHandler.getCanonicalUrl() != null)
+            {
+                String url = URLCanonicalizer.getCanonicalURL(contentHandler.getCanonicalUrl(), contextURL);
+                if (url != null && !url.equals(page.getWebURL().getURL()))
+                {
+                    page.setRedirect(true);
+                    page.setRedirectedToUrl(url);
+
+                    // Do parse the retrieved content for additional links
+                }
+            }
+
+            // Handle fragment meta tag
+            // According to:
+            // https://developers.google.com/webmasters/ajax-crawling/docs/specification
+            // When a page contains a <meta name="fragment" content="!"> tag,
+            // this indicates
+            // that the page content is loaded through AJAX and a crawler that
+            // does not
+            // parse Javascript should request the same page with
+            // _escaped_fragment_=
+            // as a query parameter to get the appropriate content.
+            String fragment = contentHandler.getMetaTags().get("fragment");
+            if (fragment != null && "!".equals(fragment))
+            {
+                String currentUrl = page.getWebURL().getURL();
+                if (!currentUrl.contains("_escaped_fragment_"))
+                {
+                    char connector = '?';
+                    if (currentUrl.contains("&"))
+                        connector = '&';
+                    String url = currentUrl + connector + "_escaped_fragment_=";
+                    page.setRedirect(true);
+                    page.setRedirectedToUrl(url);
+
+                    // Do parse the retrieved content for additional links
+                }
+            }
+
+            HtmlParseData parseData = new HtmlParseData();
+            parseData.setText(contentHandler.getBodyText().trim());
+            parseData.setTitle(metadata.get(DublinCore.TITLE));
+            parseData.setMetaTags(contentHandler.getMetaTags());
+            // Please note that identifying language takes less than 10
+            // milliseconds
+            LanguageIdentifier languageIdentifier = new LanguageIdentifier(parseData.getText());
+            page.setLanguage(languageIdentifier.getLanguage());
+
+            Set<WebURL> outgoingUrls = new HashSet<>();
+
+            String baseURL = contentHandler.getBaseUrl();
+            if (baseURL != null)
+            {
+                contextURL = baseURL;
+            }
+
+            int urlCount = 0;
+            for (ExtractedUrlAnchorPair urlAnchorPair : contentHandler.getOutgoingUrls())
+            {
+
+                String href = urlAnchorPair.getHref();
+                if ((href == null) || href.trim().isEmpty())
+                {
+                    continue;
+                }
+
+                String hrefLoweredCase = href.trim().toLowerCase();
+                if (!hrefLoweredCase.contains("javascript:") && !hrefLoweredCase.contains("mailto:")
+                        && !hrefLoweredCase.contains("@"))
+                {
+                    String url = URLCanonicalizer.getCanonicalURL(href, contextURL);
+                    if (url != null)
+                    {
+                        WebURL webURL;
+                        try
+                        {
+                            webURL = new WebURL(url);
+                        }
+                        catch (URISyntaxException e)
+                        {
+                            logger.debug("Skipping invalid URI: {}", url);
+                            continue;
+                        }
+                        webURL.setTag(urlAnchorPair.getTag());
+                        webURL.setAnchor(urlAnchorPair.getAnchor());
+                        outgoingUrls.add(webURL);
+                        urlCount++;
+                        if (urlCount > config.getMaxOutgoingLinksToFollow())
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            parseData.setOutgoingUrls(outgoingUrls);
+
+            try
+            {
+                if (page.getContentCharset() == null)
+                {
+                    parseData.setHtml(new String(page.getContentData()));
+                }
+                else
+                {
+                    parseData.setHtml(new String(page.getContentData(), page.getContentCharset()));
+                }
+
+                page.setParseData(parseData);
+            }
+            catch (UnsupportedEncodingException e)
+            {
+                logger.error("error parsing the html: " + page.getWebURL().getURL(), e);
+                throw new ParseException();
+            }
+        }
+    }
 }
--- src/main/java/edu/uci/ics/crawler4j/util/Util.java
+++ src/main/java/edu/uci/ics/crawler4j/util/Util.java
@@ -18,25 +18,77 @@
 package edu.uci.ics.crawler4j.util;
 
 import java.nio.ByteBuffer;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Yasser Ganjisaffar
  */
-public class Util {
-  
-  public static class Reference<T> {
-    public T val;
-    
-    public Reference(T val) {
-      this.val = val;
+public class Util
+{
+    static List<String> CONTENT_TYPE_WHITELIST = Arrays.asList("text/plain", // Plain
+                                                                             // text
+            "text/html", // HTML
+            "application/xhtml+xml", // XHTML
+            "application/pdf", // PDF
+            "application/msword", // .doc (Word)
+            "application/vnd.oasis.opendocument.text", // .odt (LibreOffice
+                                                       // Writer)
+            "application/vnd.oasis.opendocument.presentation", // .odp
+                                                               // (LibreOffice
+                                                               // Impress)
+            "application/vnd.sun.xml.writer", // .sxw (OpenOffice Writer)
+            "application/vnd.sun.xml.writer.global", // .sxg (OpenOffice Writer)
+            "application/vnd.sun.xml.impress", // .sxi (OpenOffice Impress)
+            "application/vnd.ms-powerpoint", // .ppt (Powerpoint)
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document", // .docx
+                                                                                       // (Word)
+            "application/vnd.openxmlformats-officedocument.presentationml.presentation" // Powerpoint
+                                                                                        // .pptx
+    );
+
+    public static class Reference<T>
+    {
+        public T val;
+
+        public Reference(T val)
+        {
+            this.val = val;
+        }
+
+        public T assign(T val)
+        {
+            return this.val = val;
+        }
+
+        public T get()
+        {
+            return val;
+        }
     }
-    
-    public T assign(T val) {
-      return this.val = val;
+
+    public static byte[] long2ByteArray(long l)
+    {
+        byte[] array = new byte[8];
+        int i;
+        int shift;
+        for (i = 0, shift = 56; i < 8; i++, shift -= 8)
+        {
+            array[i] = (byte) (0xFF & (l >> shift));
+        }
+        return array;
     }
-    
-    public T get() {
-      return val;
+
+    public static byte[] int2ByteArray(int value)
+    {
+        byte[] b = new byte[4];
+        for (int i = 0; i < 4; i++)
+        {
+            int offset = (3 - i) * 8;
+            b[i] = (byte) ((value >>> offset) & 0xFF);
+        }
+        return b;
     }
   }
 
@@ -80,46 +132,173 @@ public class Util {
     return bbuf.getLong();
   }
   
-  public static boolean hasBinaryContent(String contentType, byte [] content) {
-    String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
-
-    if (typeStr.isEmpty())
+    /**
+     * Detect the content type of a document
+     * 
+     * @param contentType
+     *            is the content type that is addressed to the document
+     * @param content
+     *            is the text of the document
+     * @return the type of the document
+     */
+    public static String getContentType(String contentType, byte[] content)
     {
+        if (hasHTMLContent(contentType, content))
+        {
+            return "Html";
+        }
+        else if (hasWhiteListContent(contentType))
+        {
+            return "WhiteList";
+        }
+        else if (hasXMLContent(contentType, content))
+        {
+            if (hasHTMLContent(contentType, content))
+                return "Html";
+            else
+                return "XML";
+        }
+        else if (hasBinaryContent(contentType, content))
+        {
+            return "Binary";
+        }
+        else if (hasPlainTextContent(contentType))
+        {
+            return "Plaintext";
+        }
+        return "Unknown";
+    }
+
+    /**
+     * Detect if the document has is of the type html
+     *
+     * @param contentType
+     *            the type where the site is addressed with
+     * @param content
+     *            the text of the site
+     * @return true if it is a html site and false when it is not
+     */
+    public static boolean hasHTMLContent(String contentType, byte[] content)
+    {
+        String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
+        try
+        {
+            String str = "";
+            if (contentType.contains("UTF-16") || content.toString().startsWith("0xFE")
+                    || content.toString().startsWith("0xFF"))
+            {
+                str = new String(content, "UTF-16");
+            }
+            else if (contentType.contains("UTF-32"))
+            {
+                str = new String(content, "UTF-32");
+            }
+            else
+            {
+                str = new String(content, "UTF-8");
+            }
+            if (str.contains("<html") || typeStr.contains("text/html"))
+                return true;
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Detect of content is of type pdf
+     *
+     * @param contentType
+     *            the type of the content
+     * @return true if the content is of type pdf and false when it is not
+     */
+
+    public static boolean hasWhiteListContent(String contentType)
+    {
+        String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
+        for (String whiteListContentType : CONTENT_TYPE_WHITELIST)
+        {
+            if (typeStr.startsWith(whiteListContentType))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Detect if the document is of the type of XML
+     *
+     * @param contentType
+     *            the type of the document
+     * @param content
+     *            the text in the document
+     * @return true if it of the type of XML
+     */
+    public static boolean hasXMLContent(String contentType, byte[] content)
+    {
+        String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
         try
         {
             // Try to parse content as UTF-8 to see if it's xml or xhtml+xml
             String str = new String(content, "UTF-8");
-            if (str.startsWith("<?xml")) // xml should be treated as binary, but xhtml shouldn't
-                return !str.contains("<html");
+            if (str.startsWith("<?xml") || typeStr.contains("xml"))
+                return true;
         }
-        catch (Exception e)
-        {}
-        
-        // No verdict - go count non-ascii characters to estimate probability on binary content
-        int nonASCII = 0;
-        int chars = Math.min(content.length, 2048);
-        for (int i = 0; i < chars; ++i)
-            if (content[i] < 32 || content[i] > 126)
-                ++nonASCII;
-        
-        // If 10% of the characters are outside of ASCII range and no content type has been specified,
-        // it's very likely that we're dealing with binary content here.
-        return (nonASCII / (double)chars) > 0.1;
+        catch (UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
     }
-    
-    // xhtml is application/xhtml+xml - xhtml can be parsed by HTML parsed, but XML should be parsed by TIKA
-    if (typeStr.contains("xml"))
-        return !typeStr.contains("html");
-    
-    return typeStr.contains("image") || typeStr.contains("audio") || typeStr.contains("video") ||
-           typeStr.contains("application");
-  }
 
-  public static boolean hasPlainTextContent(String contentType) {
-    String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
+    /**
+     * Detect if the document is of the type of binary so application or audio
+     * etc
+     * 
+     * @param contentType
+     *            the type where the document is addressed with
+     * @param content
+     *            the text on the document
+     * @return true if the document is of the type of binary and false whenever
+     *         it is not
+     */
+    public static boolean hasBinaryContent(String contentType, byte[] content)
+    {
+        String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
 
-    return typeStr.startsWith("text/plain");
-  }
+        if (typeStr.isEmpty())
+        {
+            // No verdict - go count non-ascii characters to estimate
+            // probability on binary content
+            int nonASCII = 0;
+            int chars = Math.min(content.length, 2048);
+            for (int i = 0; i < chars; ++i)
+                if (content[i] < 32 || content[i] > 126)
+                    ++nonASCII;
+
+            // If 10% of the characters are outside of ASCII range and no
+            // content type has been specified,
+            // it's very likely that we're dealing with binary content here.
+            return (nonASCII / (double) chars) > 0.1;
+        }
+
+        return typeStr.contains("image") || typeStr.contains("audio") || typeStr.contains("video")
+                || typeStr.contains("application");
+    }
+
+    /**
+     * Detect if the document is of type plain text
+     * 
+     * @param contentType
+     *            the type where the document is addressed with
+     * @return true if the document is of the type of plain text
+     */
+    public static boolean hasPlainTextContent(String contentType)
+    {
+        String typeStr = (contentType != null) ? contentType.toLowerCase() : "";
+        return typeStr.startsWith("text/plain");
+    }
 
   /**
    * Sleep for a minimum number of milliseconds, ignoring
